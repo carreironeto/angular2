399be7a57afcb55847d37ca49c486aade2a81963
carreironeto <carreironeto@hotmail.com>
2019-10-21 22:30:37 -0300

    task 0 configuracao

---
+++ cadastro/app.ts
@@ -0,0 +1,8 @@
+class OlaTypeScript {
+    constructor(public message: string) {
+        console.log('para aqui');
+    }
+}
+
+var ola = new OlaTypeScript("Felippe quase tudo certo!");
+console.log(ola.message);
---
+++ cadastro/jsScripts/app.js
@@ -0,0 +1,10 @@
+var OlaTypeScript = /** @class */ (function () {
+    function OlaTypeScript(message) {
+        this.message = message;
+        console.log('para aqui');
+    }
+    return OlaTypeScript;
+}());
+var ola = new OlaTypeScript("Felippe quase tudo certo!");
+console.log(ola.message);
+//# sourceMappingURL=app.js.map
